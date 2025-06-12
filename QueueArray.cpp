bca90ddf96940a7921044a72e9634cdab42163ae
restuchan <restu.chan188@gmail.com>
2025-06-12 11:28:01 +0700

    iterasi dari awal array hingga REAR

--- QueueArray.cpp
+++ QueueArray.cpp
@@ -107,4 +107,14 @@ class Queues
 
             FRONT_position = 0;
 
-           
+            //iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
+};
+
