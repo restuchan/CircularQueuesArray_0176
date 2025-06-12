65c6e380759642a0ac262a98d2acab3eb6f19594
restuchan <restu.chan188@gmail.com>
2025-06-12 10:12:02 +0700

    first commit

---
+++ QueueArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Queues 
+{
+    private:
+    static const int max = 5;
+    int FRONT, REAR;
+    int queue_array[5];
+
+    public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert()
+    {
+        int num;
+        cout << "Enter a number";
+        cin >> num;
+        cout << endl;
+    }
+};
