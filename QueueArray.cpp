a1797336c0a1c1d8acf6396960d369637b5198ec
restuchan <restu.chan188@gmail.com>
2025-06-12 11:21:37 +0700

    jika elemen yg dihapus berada di posisi terakhir array

--- QueueArray.cpp
+++ QueueArray.cpp
@@ -62,4 +62,14 @@ class Queues
             FRONT = -1;
             REAR = -1;
         }
-       
+        else
+        {
+            //jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT = max - 1)
+                FRONT = 0;
+            else 
+                FRONT = FRONT + 1;
+        }
+    }
+
+    
