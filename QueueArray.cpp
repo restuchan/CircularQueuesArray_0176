21c94711d9ed4b6a248d8fb730743c3f76ad1b01
restuchan <restu.chan188@gmail.com>
2025-06-12 11:21:03 +0700

    mengevek jika antrian hanya memiliki satu elemen

--- QueueArray.cpp
+++ QueueArray.cpp
@@ -56,4 +56,10 @@ class Queues
         }
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
-        
+       //cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+       
