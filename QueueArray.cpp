d5a8dd8fece7633c51c031198e5742159d0b0287
restuchan <restu.chan188@gmail.com>
2025-06-12 11:29:10 +0700

    try pilihan menu

--- QueueArray.cpp
+++ QueueArray.cpp
@@ -119,3 +119,21 @@ class Queues
 };
 
 int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try 
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display value" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            
